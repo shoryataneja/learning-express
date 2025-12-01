6d10be45e2083ae39a9fff4ac7955d4f7790fa41
shoryataneja <shoryataneja5@gmail.com>
2025-12-01 19:28:33 +0530

    making crud

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const express = require('express')
+const express = require('express');
 
-const app = express;
+const app = express();
+
+const users = [
+    {
+        'id' : 1,
+        'name' : 'John',
+        'age' : 30,
+        'mobile ': 9999999999
+    }
+]
+
+app.post("/createUser",(req,res)=>{
+    const()
+})
+
+
+
+// app.get("/hello",(req,res)=>{
+//     res.send('hello world')
+// })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
